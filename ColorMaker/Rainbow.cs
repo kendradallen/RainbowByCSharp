88f2fc9b8c85831463801cb620f6259985652e87
kendradallen <54453520+kendradallen@users.noreply.github.com>
2019-09-27 15:20:16 -0500

    Add files via upload

---
+++ ColorMaker/Rainbow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorMaker
+{
+    public class Rainbow
+    {
+        public static void MakeItGay(string text)
+        {
+            ConsoleColor[] gay = { ConsoleColor.Red, ConsoleColor.DarkYellow, ConsoleColor.Yellow, ConsoleColor.Green, ConsoleColor.Blue, ConsoleColor.DarkMagenta };
+
+            char[] test = text.ToCharArray();
+            byte box = 0;
+            for (int i = 0; i < test.Length; i++)
+            {
+                if (test[i] != ' ')
+                {
+                    Console.ForegroundColor = gay[box++ % gay.Length];
+                }
+                Console.Write(test[i]);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
